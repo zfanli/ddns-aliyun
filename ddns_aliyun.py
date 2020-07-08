66147102a5570d9c201380493afa9f769abd5287
Richard <zfanli@outlook.com>
2020-07-09 01:08:48 +0800

    Update script

--- ddns_aliyun.py
+++ ddns_aliyun.py
@@ -1,7 +1,8 @@
 import pprint
 import json
 import yaml
-import sqlite3
+
+# import sqlite3
 import requests
 
 # import api utils
@@ -31,56 +32,56 @@ def get_ip():
     return requests.get("http://ip.cip.cc").text.strip()
 
 
-def database():
-    """connect to sqlite and get a cursor to execute sql"""
-    conn = sqlite3.connect("ip.db")
-    return conn
+# def database():
+#     """connect to sqlite and get a cursor to execute sql"""
+#     conn = sqlite3.connect("ip.db")
+#     return conn
 
 
-def ensure_database():
-    """ensure ip table exists"""
-    conn = database()
-    cursor = conn.cursor()
-    cursor.execute(
-        "select count(*) from sqlite_master where type='table' and name = 'ip'"
-    )
-    if cursor.fetchall()[0][0] == 0:
-        cursor.execute(
-            """CREATE TABLE `ip` (
-                `ip` varchar(15) NOT NULL,
-                `current` var(1) NOT NULL,
-                `timestamp` datetime DEFAULT CURRENT_TIMESTAMP,
-                PRIMARY KEY (`ip`)
-            )"""
-        )
-    cursor.close()
-    return conn
+# def ensure_database():
+#     """ensure ip table exists"""
+#     conn = database()
+#     cursor = conn.cursor()
+#     cursor.execute(
+#         "select count(*) from sqlite_master where type='table' and name = 'ip'"
+#     )
+#     if cursor.fetchall()[0][0] == 0:
+#         cursor.execute(
+#             """CREATE TABLE `ip` (
+#                 `ip` varchar(15) NOT NULL,
+#                 `current` var(1) NOT NULL,
+#                 `timestamp` datetime DEFAULT CURRENT_TIMESTAMP,
+#                 PRIMARY KEY (`ip`)
+#             )"""
+#         )
+#     cursor.close()
+#     return conn
 
 
-def is_current_ip(conn, ip):
-    cursor = conn.cursor()
-    cursor.execute(f"select count(*) from ip where ip='{ip}' and current='1'")
-    result = cursor.fetchall()[0][0] == 1
-    cursor.close()
-    return result
+# def is_current_ip(conn, ip):
+#     cursor = conn.cursor()
+#     cursor.execute(f"select count(*) from ip where ip='{ip}' and current='1'")
+#     result = cursor.fetchall()[0][0] == 1
+#     cursor.close()
+#     return result
 
 
-def write_new_ip(conn, ip):
-    cursor = conn.cursor()
-    cursor.execute("UPDATE ip SET current='0' WHERE current='1'")
-    cursor.execute(f"INSERT INTO ip (IP, CURRENT) VALUES('{ip}', '1')")
-    conn.commit()
-    cursor.close()
+# def write_new_ip(conn, ip):
+#     cursor = conn.cursor()
+#     cursor.execute("UPDATE ip SET current='0' WHERE current='1'")
+#     cursor.execute(f"INSERT INTO ip (IP, CURRENT) VALUES('{ip}', '1')")
+#     conn.commit()
+#     cursor.close()
 
 
-def is_ip_changed(ip):
-    conn = ensure_database()
-    result = False
-    if not is_current_ip(conn, ip):
-        write_new_ip(conn, ip)
-        result = True
-    conn.close()
-    return result
+# def is_ip_changed(ip):
+#     conn = ensure_database()
+#     result = False
+#     if not is_current_ip(conn, ip):
+#         write_new_ip(conn, ip)
+#         result = True
+#     conn.close()
+#     return result
 
 
 def query_domain_records(client):
@@ -121,11 +122,20 @@ if __name__ == "__main__":
     client = make_client()
 
     ip = get_ip()
-    if is_ip_changed(ip):
-        # query domain records
-        records = query_domain_records(client)
-        records = json.loads(records)["DomainRecords"]["Record"]
-        for r in records:
-            if r["RR"] in rr_list:
-                update_domain_record(client, ip, r)
-                print(r["RR"])
+    # if is_ip_changed(ip):
+    #     # query domain records
+    #     records = query_domain_records(client)
+    #     records = json.loads(records)["DomainRecords"]["Record"]
+    #     for r in records:
+    #         if r["RR"] in rr_list:
+    #             update_domain_record(client, ip, r)
+    #             print(r["RR"])
+
+    # query domain records
+    records = query_domain_records(client)
+    records = json.loads(records)["DomainRecords"]["Record"]
+    for r in records:
+        if r["RR"] in rr_list and r["Value"] != ip:
+            update_domain_record(client, ip, r)
+            print(r["RR"])
+
