50591396ac72a6f60b626220a2ee2985ecdb9ce5
Richard <zfanli@outlook.com>
2020-10-09 01:04:42 +0800

    Update command

--- dns_challenge.sh
+++ dns_challenge.sh
@@ -1,4 +1,4 @@
 echo python3 $(dirname $0)/dns_challenge_aliyun.py ${CERTBOT_VALIDATION}
-python3 dns_challenge_aliyun.py ${CERTBOT_VALIDATION}
+python3 $(dirname $0)/dns_challenge_aliyun.py ${CERTBOT_VALIDATION}
 # sleep to make sure the challenge is applied
 sleep 25
